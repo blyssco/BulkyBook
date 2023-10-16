b62c3dc9c16866a6f01ae72b9e495460f7fbb33e
youssefboulaich00@gmail.com <youssefboulaich00@gmail.com>
2023-10-16 16:47:36 +0200

    Add project files.

---
+++ BulkyBookWeb/wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿const element = document.getElementById("ajs");
+
+const ChangeColorAfterDelay = function () {
+    element.style.backgroundColor = "yellow";
+};
+
+setTimeout(ChangeColorAfterDelay, 4000);
